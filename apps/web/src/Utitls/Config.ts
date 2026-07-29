6d586f46382882ad12d82cc66d479643e9617a0e
JOhn_Mustard <166864125+aditya-wuw@users.noreply.github.com>
2026-07-30 00:52:08 +0530

    Button fix & backend version update

--- apps/web/src/Utitls/Config.ts
+++ apps/web/src/Utitls/Config.ts
@@ -3,7 +3,7 @@ export const AppName = "copy-chan";
 export interface VersionsType {
   Version: string;
   Date: string;
-  PatchNotes: string;
+  PatchNotes: string | undefined;
 }
 
 export const Socials = {
@@ -28,7 +28,7 @@ export const Versions: VersionsType[] = [
   {
     Version: "0.1.0-preview.7292026",
     Date: "29th Jul 2026",
-    PatchNotes: "https://github.com/Smug-Cat-s-Den/Copy-Chan/issues/52",
+    PatchNotes: "https://github.com/Smug-Cat-s-Den/Copy-Chan/issues/57",
   },
   {
     Version: "0.0.9",
